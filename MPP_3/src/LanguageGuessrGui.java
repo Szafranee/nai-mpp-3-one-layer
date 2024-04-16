347e8ba2da0f30c43030c8a956273ee35d9c6e67
Kacper Szafrański <kacperr.szafranski@gmail.com>
2024-04-16 23:18:51 +0200

    Add Gui prototype

---
+++ MPP_3/src/LanguageGuessrGui.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class LanguageGuessrGui {
+    private JPanel panel1;
+    private JLabel Title;
+    private JTextArea textInputArea;
+    private JPanel textAreaPanel;
+    private JPanel infoPanel;
+}
--- MPP_3/src/Main.java
+++ MPP_3/src/Main.java
@@ -1,8 +1,11 @@
+import javax.swing.*;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<String> languagesList = FileHandler.getLanguages("data");
         List<Vector> vectorsList = FileHandler.createLanguageVectorsFromFiles("data", languagesList);
+
     }
+
 }
