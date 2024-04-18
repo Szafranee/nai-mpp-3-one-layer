7e85f84601bdf013651f35ebc52911eed4d65803
Kacper Szafrański <kacperr.szafranski@gmail.com>
2024-04-18 09:36:37 +0200

    Delete LanguageGuessrGui.java

--- MPP_3/src/LanguageGuessrGui.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class LanguageGuessrGui {
-    private JPanel panel1;
-    private JLabel Title;
-    private JTextArea textInputArea;
-    private JPanel textAreaPanel;
-    private JPanel infoPanel;
-}
